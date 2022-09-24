967d04b450f6c6f6ffdd8225620158282dd2b5ea
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2022-09-24 17:52:12 +0300

    add start task_3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import { Profile } from 'components/Profile/Profile';
 import user from 'path/user';
 import { Statistics } from 'components/Statistics/Statistics';
 import data from 'path/data';
+import { FriendList } from 'components/FriendList/FriendList';
+import friends from 'path/friends';
 
 export const App = () => {
   return (
@@ -14,6 +16,7 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
       {/* <Statistics stats={data} /> */}
     </div>
   );
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import { FriendListItem } from './FriendListItem';
+import css from './Friend.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friend}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+};
